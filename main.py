61765c372a0cca5854fb98111094ef3a1a3968d0
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-07-08 22:33:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import re
 import math
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 from queue import PriorityQueue
 from typing import Callable, Dict, DefaultDict, Optional
 from collections import defaultdict
@@ -22,40 +22,28 @@ with open("input.txt", "r") as f:
 start, goal, limit
 
 # %%
-img = ImageOps.grayscale(Image.open("./img/map.bmp"))
-map = np.array(img).astype(int)
-
-
-# %%
-def real_cost(map: np.ndarray, from_pos: Point, to_pos: Point) -> float:
-    x1, y1 = from_pos
-    x2, y2 = to_pos
-    delta = map[to_pos] - map[from_pos]
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) + (
-        0.5 * np.sign(delta) + 1
-    ) * abs(delta)
-
-
-real_cost(map, start, goal)
+img = Image.open("./img/map.bmp")
+grayscale = ImageOps.grayscale(Image.open("./img/map.bmp"))
+map = np.array(grayscale).astype(int)
 
 # %%
-def in_bounds_constraint(obj, cur, neighbor, **kargs) -> bool:
-    x, y = neighbor
-    x_limit, y_limit = obj.map.shape
-    return 0 <= x < x_limit and 0 <= y < y_limit
-
-
-def limit_constraint(obj, cur, neighbor, **kargs) -> bool:
+def limit_constraint(obj, cur: Point, neighbor: Point, **kargs) -> bool:
     limit = kargs["limit"]
     map = obj.map
     return abs(map[neighbor] - map[cur]) <= limit
+
+
+def in_bounds_constraint(obj, neighbor: Point) -> bool:
+    x, y = neighbor
+    x_limit, y_limit = obj.map.shape
+    return 0 <= x < x_limit and 0 <= y < y_limit
 
 
 def grid_neighbors(obj, cur: Point):
     neighbors = []
     for move in obj.moveset:
         neighbor = tuple(i + j for i, j in zip(cur, move))
-        if in_bounds_constraint(obj, cur, neighbor) and all(
+        if in_bounds_constraint(obj, neighbor) and all(
             f(obj, cur, neighbor, **kwargs) for f, kwargs in obj.constraint
         ):
             neighbors.append(neighbor)
@@ -80,22 +68,36 @@ class AStar:
         self.moveset = moveset
         self.constraint = custom_constraint
 
-    def search(self, start: Point, goal: Point):
+    def reconstruct_path(self, came_from, start, goal, result_order_from_start):
+        path = []
+        cur = goal
+        while cur != start:
+            path.append(cur)
+            cur = came_from[cur]
+        path.append(start)
+        if result_order_from_start:
+            path.reverse()
+        return path
+
+    def search(self, start: Point, goal: Point, result_order_from_start: bool = False):
         open_set = PriorityQueue()
         came_from: Dict[Point, Optional[Point]] = {start: None}
         g_cost: DefaultDict[Point, float] = defaultdict(lambda: np.inf)
         f_cost: Dict[Point, float] = {}
+
         open_set.put([0, start])
         g_cost[start] = 0
         f_cost[start] = 0
 
-        counter = 0
-
         while not open_set.empty():
             cur = open_set.get()[1]
-            print(cur)
             if cur == goal:
-                return "ADD RECONSTRUCT PATH FUNCTION HERE"
+                return [
+                    self.reconstruct_path(
+                        came_from, start, goal, result_order_from_start
+                    ),
+                    f_cost[goal],
+                ]
             for next in self.neighbors(self, cur):
                 new_g_cost = g_cost[cur] + self.g(self.map, cur, next)
                 if new_g_cost < g_cost[next]:
@@ -103,16 +105,23 @@ class AStar:
                     f_cost[next] = new_g_cost + self.h(self.map, next, goal)
                     came_from[next] = cur  # Set "next neighbor" parent to cur
                     open_set.put([f_cost[next], next])
-
-            # if counter == 200:
-            #     return "TIMEOUT"
-            # else:
-            #     counter += 1
-
         # Open set is empty but goal was never reached
         return "LOL FAILED"
 
 
+# %%
+def real_cost(map: np.ndarray, from_pos: Point, to_pos: Point) -> float:
+    x1, y1 = from_pos
+    x2, y2 = to_pos
+    delta = map[to_pos] - map[from_pos]
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) + (
+        0.5 * np.sign(delta) + 1
+    ) * abs(delta)
+
+
+real_cost(map, start, goal)
+
+#%%
 custom_constraint = [(limit_constraint, {"limit": limit})]
 a_star = AStar(
     map,
@@ -128,8 +137,9 @@ a_star = AStar(
 import time
 
 start_time = time.time()
-print(a_star.search(start, goal))
+path, total_cost = a_star.search(start, goal)
 stop_time = time.time()
 hours, rem = divmod(stop_time - start_time, 3600)
 minutes, seconds = divmod(rem, 60)
-print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+print("Elapsed: {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+print(f"Total cost: {total_cost}")
